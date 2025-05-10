5e28b1690578d97571032adc3b6ad24319b2aee8
Leonid <leonigleo13@gmail.com>
2025-05-10 22:37:21 +0300

    Second step 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("io.freefair.lombok") version "8.6"
+	checkstyle
 	application
 }
 
@@ -23,9 +25,14 @@ repositories {
 }
 
 dependencies {
+	implementation("org.projectlombok:lombok:1.18.32")
+	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+	annotationProcessor("org.projectlombok:lombok:1.18.32")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
---
+++ app/src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,44 @@
+package hexlet.code.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String passwordDigest;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updateAt;
+}
