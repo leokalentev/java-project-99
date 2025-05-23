ffc35ee8c96a90f3145d9390343cdf1d774a3c3c
Leonid <leonigleo13@gmail.com>
2025-05-23 20:06:13 +0300

    Before 10 step 3.0

--- app/src/main/java/hexlet/code/component/DotenvLoader.java
+++ app/src/main/java/hexlet/code/component/DotenvLoader.java
@@ -1,6 +1,7 @@
 package hexlet.code.component;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -8,14 +9,16 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("!test")
 public class DotenvLoader {
-
     @PostConstruct
     public void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("SENTRY_DSN", dotenv.get("SENTRY_DSN"));
-        System.setProperty("SENTRY_ENVIRONMENT", dotenv.get("SENTRY_ENVIRONMENT"));
-        System.out.println(".env переменные загружены");
+        try {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("SENTRY_DSN", dotenv.get("SENTRY_DSN"));
+            System.setProperty("SENTRY_ENVIRONMENT", dotenv.get("SENTRY_ENVIRONMENT", "development"));
+            System.out.println(".env переменные загружены");
+        } catch (DotenvException ex) {
+            System.out.println("Не удалось загрузить .env: " + ex.getMessage());
+        }
     }
 }
 
