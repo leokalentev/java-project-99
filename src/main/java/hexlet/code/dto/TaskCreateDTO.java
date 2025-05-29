abe7eb09a9f9f0705d20099fb16568f3ddc17779
Leonid <leonigleo13@gmail.com>
2025-05-29 18:55:50 +0300

    Tenth step: update TaskCreateDTO

--- src/main/java/hexlet/code/dto/TaskCreateDTO.java
+++ src/main/java/hexlet/code/dto/TaskCreateDTO.java
@@ -2,6 +2,7 @@ package hexlet.code.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,7 @@ public class TaskCreateDTO {
     private Integer index;
     private String content;
 
+    @NotNull
     @JsonProperty("assignee_id")
     private Long assigneeId;
 
