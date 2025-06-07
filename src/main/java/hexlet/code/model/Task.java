272df753f0120308715868c61e6bd71e7c775f3d
Leonid <leonigleo13@gmail.com>
2025-06-07 17:44:18 +0300

    Tenth step: update Task

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -45,7 +45,7 @@ public class Task {
     @JoinColumn(name = "task_status_id")
     private TaskStatus taskStatus;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     @JoinColumn(name = "assignee_id")
     private User assignee;
 
