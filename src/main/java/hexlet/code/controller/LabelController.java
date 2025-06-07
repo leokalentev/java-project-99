aa9fe504006dfd0c6e4ba79105c62967d333f8a3
Leonid <leonigleo13@gmail.com>
2025-06-07 17:01:38 +0300

    Tenth step delete ex import

--- src/main/java/hexlet/code/controller/LabelController.java
+++ src/main/java/hexlet/code/controller/LabelController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -7,16 +7,13 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
